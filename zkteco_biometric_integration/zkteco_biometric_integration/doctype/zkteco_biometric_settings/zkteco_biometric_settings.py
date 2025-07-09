798c5e76e512226d5a4f98fcf26b050311b686b4
flexGURU <mukunajohn329@gmail.com>
2025-07-09 13:53:06 +0300

    feat: add token generation functionality

--- zkteco_biometric_integration/zkteco_biometric_integration/doctype/zkteco_biometric_settings/zkteco_biometric_settings.py
+++ zkteco_biometric_integration/zkteco_biometric_integration/doctype/zkteco_biometric_settings/zkteco_biometric_settings.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, Navari Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import requests
 from frappe.model.document import Document
 
 
 class ZKTecoBiometricSettings(Document):
-	pass
+	def generate_token(self):
+		headers = {"Content-Type": "application/json"}
+
+		payload = {"username": self.username, "password": self.password}
+		self.url_path = "/jwt-api-token-auth/"
+
+		endpoint_url = f"{self.url}{self.url_path}"
+
+		try:
+			response = requests.post(endpoint_url, payload, headers)
+
+			if response.status_code == 200:
+				print("jwt token successful", response.text)
+				return response.json().get("token")
+			else:
+				frappe.throw(f"Problem generating Token {response.json().get("detail")}")
+
+		except Exception as e:
+			frappe.throw("Problem generating Token", str(e))
