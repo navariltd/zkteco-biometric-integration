e3e93315fa9e7949ac5cd0c9b09b644528d2231a
flexGURU <mukunajohn329@gmail.com>
2025-07-09 13:45:50 +0300

    feat: add settings doctype

---
+++ zkteco_biometric_integration/zkteco_biometric_integration/doctype/zkteco_biometric_settings/zkteco_biometric_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Navari Limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ZKTeco Biometric Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
