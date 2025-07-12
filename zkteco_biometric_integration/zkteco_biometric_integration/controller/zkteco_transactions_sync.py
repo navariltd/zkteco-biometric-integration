9cc744cd10d603c419ac246e4385b9b5229d4f64
flexGURU <mukunajohn329@gmail.com>
2025-07-12 14:04:09 +0300

    feat: map transactions response data to employee checkin doctype

--- zkteco_biometric_integration/zkteco_biometric_integration/controller/zkteco_transactions_sync.py
+++ zkteco_biometric_integration/zkteco_biometric_integration/controller/zkteco_transactions_sync.py
@@ -43,3 +43,24 @@ def get_configs(username):
 		return token, url
 
 	return token, url
+
+
+@frappe.whitelist(allow_guest=True)
+def handle_employee_checkin(username):
+	transactions = get_transactions(username)
+
+	for transaction in transactions:
+		employee = transaction.get("emp_code")
+		log_type = "IN" if transaction["punch_state_display"] == "Check-In" else "OUT"
+		punch_time = transaction.get("punch_time")
+
+		employee_checkin = frappe.get_doc(
+			{
+				"doctype": "Employee Checkin",
+				"employee": employee,
+				"log_type": log_type,
+				"time": punch_time,
+			}
+		)
+
+		employee_checkin.save()
