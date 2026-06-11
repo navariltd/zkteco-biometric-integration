a0fff26ac656559c5b5464d0ed3865724b3869f7
craizytech <craizytech@gmail.com>
2026-06-11 22:04:06 +0300

    fixed ruff linter issues

--- zkteco_biometric_integration/zkteco_biometric_integration/api/test_utils.py
+++ zkteco_biometric_integration/zkteco_biometric_integration/api/test_utils.py
@@ -114,9 +114,7 @@ def cleanup_employee():
 
 def ensure_company() -> str:
 	if not frappe.db.exists("Warehouse Type", "Transit"):
-		frappe.get_doc({"doctype": "Warehouse Type", "name": "Transit"}).insert(
-			ignore_permissions=True
-		)
+		frappe.get_doc({"doctype": "Warehouse Type", "name": "Transit"}).insert(ignore_permissions=True)
 
 	if not frappe.db.exists("Company", TEST_COMPANY_NAME):
 		frappe.get_doc(
