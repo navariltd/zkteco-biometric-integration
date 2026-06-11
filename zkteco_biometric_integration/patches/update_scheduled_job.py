b4e8e3f6be5b2f26063d9906e0debb0dc81c3907
craizytech <craizytech@gmail.com>
2026-06-11 20:59:38 +0300

    style(lint): sort imports and reformat app-level and patch files

--- zkteco_biometric_integration/patches/update_scheduled_job.py
+++ zkteco_biometric_integration/patches/update_scheduled_job.py
@@ -1,39 +1,32 @@
 import frappe
-from zkteco_biometric_integration.zkteco_biometric_integration import (
-    SCHEDULED_JOB_METHOD,
-)
 from frappe.model.document import Document
+
+from zkteco_biometric_integration.zkteco_biometric_integration import (
+	SCHEDULED_JOB_METHOD,
+)
 from zkteco_biometric_integration.zkteco_biometric_integration.doctype.zkteco_global.zkteco_global import (
-    ZKTecoGlobal,
+	ZKTecoGlobal,
 )
 
 
 def execute():
-    update_scheduled_job()
+	update_scheduled_job()
 
 
 def get_current_frequency_settings() -> ZKTecoGlobal:
-
-    return frappe.get_doc("ZKTeco Global")
+	return frappe.get_doc("ZKTeco Global")
 
 
 def update_scheduled_job() -> None:
+	setting_doc = get_current_frequency_settings()
 
-    setting_doc = get_current_frequency_settings()
+	new_frequency = setting_doc.cron_expression if setting_doc.is_cron else setting_doc.fetch_frequency
 
-    new_frequency = (
-        setting_doc.cron_expression
-        if setting_doc.is_cron
-        else setting_doc.fetch_frequency
-    )
-
-    if job_doc_name := frappe.db.exists(
-        "Scheduled Job Type", {"method": SCHEDULED_JOB_METHOD}
-    ):
-        job_doc = frappe.get_doc("Scheduled Job Type", job_doc_name)
-        if setting_doc.fetch_frequency == job_doc.frequency:
-            return
-        data = {"frequency": new_frequency}
-        if setting_doc.is_cron:
-            data["cron_expression"] = setting_doc.cron_expression
-        job_doc.db_set(data, update_modified=False)
+	if job_doc_name := frappe.db.exists("Scheduled Job Type", {"method": SCHEDULED_JOB_METHOD}):
+		job_doc = frappe.get_doc("Scheduled Job Type", job_doc_name)
+		if setting_doc.fetch_frequency == job_doc.frequency:
+			return
+		data = {"frequency": new_frequency}
+		if setting_doc.is_cron:
+			data["cron_expression"] = setting_doc.cron_expression
+		job_doc.db_set(data, update_modified=False)
