89c97209cd5e9b135f378077b7712436ef5fcb0f
craizytech <craizytech@gmail.com>
2026-06-17 21:19:53 +0300

    fixed the function signature to match correct method behaviour

--- zkteco_biometric_integration/__init__.py
+++ zkteco_biometric_integration/__init__.py
@@ -3,7 +3,7 @@ import frappe
 __version__ = "0.0.1"
 
 
-def is_website_user(username: str | None = None) -> str | None:
+def is_website_user(username: str | None = None) -> bool:
 	return frappe.db.get_value("User", username or frappe.session.user, "user_type") == "Website User"
 
 
